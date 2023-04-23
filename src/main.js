4da725828038953fd4df1ee8ee1430eafa8def5a
joohwan9607 <joohwan9607@mobility42.io>
2023-04-24 02:25:46 +0900

    axios Test - peanutFriends api 받아오기

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import {createRouter, createWebHistory} from 'vue-router'
+//
+// const routes = [
+//     {path:'/', component: App}
+// ]
+
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes
+// })
+
 
 createApp(App).mount('#app')
