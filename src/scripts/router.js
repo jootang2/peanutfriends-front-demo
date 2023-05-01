8e24117443e6e7574b54949a192fbe3dcbb31864
joohwan9607 <joohwan9607@mobility42.io>
2023-05-01 20:20:17 +0900

    web storm chatting - 안됨

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,10 +1,12 @@
 import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
+import Chatting from "@/pages/Chatting.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
-    {path:'/', component: Home},
-    {path: '/login', component: Login}
+    {path:'/home', component: Home},
+    {path: '/login', component: Login},
+    {path: '/', component: Chatting}
 ]
 
 const router = createRouter({
